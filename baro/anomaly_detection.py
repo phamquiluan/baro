ce5d604cc05078d2ac166ef957b0e014a20522a6
Luan Pham <phamquiluan@gmail.com>
2024-04-17 02:52:15 +1000

    add Nsigma

--- baro/anomaly_detection.py
+++ baro/anomaly_detection.py
@@ -1,7 +1,26 @@
 import pandas 
 
-def nsigma(data, k=3):
-    pass
+def nsigma(data, k=3, startsfrom=100):
+    """For each time series (column) in the data,
+    detect anomalies using the n-sigma rule.
+    Return the timestamps of the anomalies.
+    """
+    anomalies = []
+    for col in data.columns:
+        if col == "time":
+            continue
+        # for each timestep starts from `startsfrom`,
+        # calculate the mean and standard deviation
+        # of the all past timesteps
+        for i in range(startsfrom, len(data)):
+            mean = data[col].iloc[:i].mean()
+            std = data[col].iloc[:i].std()
+            if abs(data[col].iloc[i] - mean) > k * std:
+                anomalies.append(data['time'].iloc[i])
+    return anomalies
+
+
+
 
 def bocpd(data):
     # TODO: Implement Bayesian Online Change Point Detection
