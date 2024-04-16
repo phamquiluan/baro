f5be293d35a348418fa50093ca33fb80df1293dd
Luan Pham <phamquiluan@gmail.com>
2024-04-16 17:15:22 +0000

    add test file

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@
 from os import path
 
 import numpy as np
+import pandas as pd
 import pytest
 
 
@@ -10,5 +11,14 @@ from baro.anomaly_detection import nsigma
 def test_basic():
     """Test basic."""
     time_col = np.arange(0, 1000, 1)
-    x_col = np.random.normal(0, 1, 1000)
+    normal_latency = np.random.normal(5, 1, 500)
+    abnormal_latency = np.random.normal(50, 1, 500)
+    latency = np.concatenate((normal_latency, abnormal_latency))
+    
+    # make df from time_col and latency
+    df = pd.DataFrame({'time': time_col, 'latency': latency})
+    anomalies = nsigma(df)
+    print(anomalies)
+
+     
 
