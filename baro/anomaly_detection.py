5511e944e441f25c47869ca47bfbf482b575554b
Luan Pham <phamquiluan@gmail.com>
2024-04-17 02:48:00 +1000

    update ad

--- baro/anomaly_detection.py
+++ baro/anomaly_detection.py
@@ -1,3 +1,16 @@
 import pandas 
 
-# TODO: add anomaly detector here
+def nsigma(data, k=3):
+    pass
+
+def bocpd(data):
+    # TODO: Implement Bayesian Online Change Point Detection
+    raise NotImplementedError
+
+def anomaly_detector(data, method="nsigma"):
+    # assert data is dataframe
+    assert isinstance(data, pandas.DataFrame)
+
+    # retain all `latency` and `error` columns
+    data = data[['latency', 'error']]
+    
