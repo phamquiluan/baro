830fe196e0dc68c84de6dda0ba238e8d38c04003
Luan Pham <phamquiluan@gmail.com>
2024-04-17 15:04:04 +0000

    update anomaly detection

--- baro/anomaly_detection.py
+++ baro/anomaly_detection.py
@@ -20,17 +20,20 @@ def nsigma(data, k=3, startsfrom=100):
     return anomalies
 
 
-def find_anomalies(data, threshold=0.01):
+def find_anomalies(data, df=None,threshold=0.01):
+    assert "time" in df.columns
     anomalies = []
     for i in range(1, len(data)):
         if data[i] > threshold:
-            anomalies.append(i)
+            # anomalies.append(i)
+            anomalies.append(df['time'].iloc[i])
 
     # re-try if threshold doesn't work
     if len(anomalies) == 0:
         head = 5
         data = data[head:]
-        anomalies = [np.argmax(data) + head]
+        # anomalies = [np.argmax(data) + head]
+        anomalies = [df['time'].iloc[np.argmax(data) + head]]
 
     # merge continuous anomalies if the distance are shorter than 5 steps
     merged_anomalies = [] if len(anomalies) == 0 else [anomalies[0]]
@@ -43,10 +46,32 @@ def find_anomalies(data, threshold=0.01):
 
 def bocpd(data):
     from functools import partial
-    from baro._bocpd import online_changepoint_detection, constant_hazard
-    R, maxes = online_changepoint_detection(
-        data, hazard_function, online_ll.StudentT(alpha=0.1, beta=.01, kappa=1, mu=0)
-    )
+    from baro._bocpd import online_changepoint_detection, constant_hazard, MultivariateT
+
+    # "    data = pd.read_csv(data_path)   \n",
+    # "    selected_cols = [c for c in data.columns if \"latency-50\" in c]\n",
+    # "    data = data[selected_cols]\n",
+    # "    data = data.fillna(method=\"ffill\")\n",
+    # "    data = data.fillna(0)\n",
+    # "\n",
+    anomalies = []
+    for c in data.columns:
+        if col == "time":
+            continue
+        data[c] = (data[c] - np.min(data[c])) / (np.max(data[c]) - np.min(data[c]))
+        data = data.fillna(method="ffill")
+        data = data.fillna(0)
+        data = data.to_numpy()
+
+        R, maxes = online_changepoint_detection(
+            data,
+            partial(constant_hazard, 50),
+            MultivariateT(dims=data.shape[1])
+        )
+        Nw = 10
+        anomalies.extend(find_anomalies(data=R[Nw,Nw:-1].tolist(), df=data))
+    return anomalies
+    
 
 def anomaly_detector(data, method="nsigma"):
     # assert data is dataframe
