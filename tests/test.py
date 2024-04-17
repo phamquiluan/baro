7be4d99250116530a7ed50d5044e7f245cbc2e50
Luan Pham <phamquiluan@gmail.com>
2024-04-17 15:13:59 +0000

    update tests

--- tests/test.py
+++ tests/test.py
@@ -24,16 +24,16 @@ def test_nsigma_basic():
      
 def test_bocpd_basic():
     """Test bocpd basic."""
-    time_col = np.arange(0, 1000, 1)
-    normal_latency = np.random.normal(3, 1, 500)
+    time_col = np.arange(0, 200, 1)
+    normal_latency = np.random.normal(3, 1, 100)
     normal_latency = np.clip(normal_latency, 1, 5)    
     
-    abnormal_latency = np.random.normal(50, 1, 500)
+    abnormal_latency = np.random.normal(50, 1, 100)
     latency = np.concatenate((normal_latency, abnormal_latency))
     
     # make df from time_col and latency
     df = pd.DataFrame({'time': time_col, 'latency': latency})
     
     anomalies = bocpd(df)
-    print(anomalies)
+    assert abs(anomalies[0] - 100) < 10, anomalies
 test_bocpd_basic()
