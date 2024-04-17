833291d406264f343c181dbbdb7ce3c90302d66b
Luan Pham <phamquiluan@gmail.com>
2024-04-17 22:37:40 +1000

    update bocpd

--- baro/_bocpd.py
+++ baro/_bocpd.py
@@ -1,4 +1,4 @@
-# The code is copied and developed from 
+# The code is shamelessly copied and developed from 
 # https://github.com/hildensia/bayesian_changepoint_detection/tree/master/bayesian_changepoint_detection
 from abc import ABC, abstractmethod
 
@@ -133,6 +133,41 @@ class MultivariateT(BaseLikelihood):
         )
         self.dof = np.concatenate([self.dof[:1], self.dof + 1])
         self.kappa = np.concatenate([self.kappa[:1], self.kappa + 1])
+
+
+def const_prior(t, p: float = 0.25):
+    """
+    Constant prior for every datapoint
+    Arguments:
+        p - probability of event
+    """
+    return np.log(p)
+
+
+def geom_prior(t, p: float = 0.25):
+    """
+    geometric prior for every datapoint
+    Refer to https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.geom.html for more information on the geometric prior
+    Everything reported is in log form.
+    Arguments:
+        t - number of trials
+        p - probability of success
+    """
+    return np.log(ss.geom.pmf(t, p=p))
+
+
+def negative_binomial_prior(t, k: int = 1, p: float = 0.25):
+    """
+    negative binomial prior for the datapoints
+     Refer to https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.nbinom.html for more information on the geometric prior
+    Everything reported is in log form.
+
+    Parameters:
+        k - the number of trails until success
+        p - the prob of success
+    """
+
+    return ss.nbinom.pmf(self.k, t, self.p)
 
 
 def constant_hazard(lam, r):
