456636f2d241050794825a2039ed95c6b75dbcce
Luan Pham <phamquiluan@gmail.com>
2024-04-17 22:28:41 +1000

    Add MultivariateT

--- baro/_bocpd.py
+++ baro/_bocpd.py
@@ -1,6 +1,139 @@
 # The code is copied and developed from 
 # https://github.com/hildensia/bayesian_changepoint_detection/tree/master/bayesian_changepoint_detection
+from abc import ABC, abstractmethod
+
 import numpy as np
+import scipy.stats as ss
+from itertools import islice
+from numpy.linalg import inv
+
+
+class BaseLikelihood(ABC):
+    """
+    This is an abstract class to serve as a template for future users to mimick
+    if they want to add new models for online bayesian changepoint detection.
+
+    Make sure to override the abstract methods to do which is desired.
+    Otherwise you will get an error.
+
+    Update theta has **kwargs to pass in the timestep iteration (t) if desired.
+    To use the time step add this into your update theta function:
+        timestep = kwargs['t']
+    """
+
+    @abstractmethod
+    def pdf(self, data: np.array):
+        raise NotImplementedError(
+            "PDF is not defined. Please define in separate class to override this function."
+        )
+
+    @abstractmethod
+    def update_theta(self, data: np.array, **kwargs):
+        raise NotImplementedError(
+            "Update theta is not defined. Please define in separate class to override this function."
+        )
+
+
+class MultivariateT(BaseLikelihood):
+    def __init__(
+        self,
+        dims: int = 1,
+        dof: int = 0,
+        kappa: int = 1,
+        mu: float = -1,
+        scale: float = -1,
+    ):
+        """
+        Create a new predictor using the multivariate student T distribution as the posterior predictive.
+            This implies a multivariate Gaussian distribution on the data, a Wishart prior on the precision,
+             and a Gaussian prior on the mean.
+             Implementation based on Haines, T.S., Gaussian Conjugate Prior Cheat Sheet.
+        :param dof: The degrees of freedom on the prior distribution of the precision (inverse covariance)
+        :param kappa: The number of observations we've already seen
+        :param mu: The mean of the prior distribution on the mean
+        :param scale: The mean of the prior distribution on the precision
+        :param dims: The number of variables
+        """
+        # We default to the minimum possible degrees of freedom, which is 1 greater than the dimensionality
+        if dof == 0:
+            dof = dims + 1
+        # The default mean is all 0s
+        if mu == -1:
+            mu = [0] * dims
+        else:
+            mu = [mu] * dims
+
+        # The default covariance is the identity matrix. The scale is the inverse of that, which is also the identity
+        if scale == -1:
+            scale = np.identity(dims)
+        else:
+            scale = np.identity(scale)
+
+        # Track time
+        self.t = 0
+
+        # The dimensionality of the dataset (number of variables)
+        self.dims = dims
+
+        # Each parameter is a vector of size 1 x t, where t is time. Therefore each vector grows with each update.
+        self.dof = np.array([dof])
+        self.kappa = np.array([kappa])
+        self.mu = np.array([mu])
+        self.scale = np.array([scale])
+
+    def pdf(self, data: np.array):
+        """
+        Returns the probability of the observed data under the current and historical parameters
+        Parmeters:
+            data - the datapoints to be evaualted (shape: 1 x D vector)
+        """
+        self.t += 1
+        t_dof = self.dof - self.dims + 1
+        expanded = np.expand_dims((self.kappa * t_dof) / (self.kappa + 1), (1, 2))
+        ret = np.empty(self.t)
+        try:
+            # This can't be vectorised due to https://github.com/scipy/scipy/issues/13450
+            for i, (df, loc, shape) in islice(
+                enumerate(zip(t_dof, self.mu, inv(expanded * self.scale))), self.t
+            ):
+                ret[i] = ss.multivariate_t.pdf(x=data, df=df, loc=loc, shape=shape)
+        except AttributeError:
+            raise Exception(
+                "You need scipy 1.6.0 or greater to use the multivariate t distribution"
+            )
+        return ret
+
+    def update_theta(self, data: np.array, **kwargs):
+        """
+        Performs a bayesian update on the prior parameters, given data
+        Parmeters:
+            data - the datapoints to be evaluated (shape: 1 x D vector)
+        """
+        centered = data - self.mu
+
+        # We simultaneously update each parameter in the vector, because following figure 1c of the BOCD paper, each
+        # parameter for a given t, r is derived from the same parameter for t-1, r-1
+        # Then, we add the prior back in as the first element
+        self.scale = np.concatenate(
+            [
+                self.scale[:1],
+                inv(
+                    inv(self.scale)
+                    + np.expand_dims(self.kappa / (self.kappa + 1), (1, 2))
+                    * (np.expand_dims(centered, 2) @ np.expand_dims(centered, 1))
+                ),
+            ]
+        )
+        self.mu = np.concatenate(
+            [
+                self.mu[:1],
+                (np.expand_dims(self.kappa, 1) * self.mu + data)
+                / np.expand_dims(self.kappa + 1, 1),
+            ]
+        )
+        self.dof = np.concatenate([self.dof[:1], self.dof + 1])
+        self.kappa = np.concatenate([self.kappa[:1], self.kappa + 1])
+
 
 def constant_hazard(lam, r):
     """
@@ -11,7 +144,7 @@ def constant_hazard(lam, r):
     """
     return 1 / lam * np.ones(r.shape)
 
-    
+
 def online_changepoint_detection(data, hazard_function, log_likelihood_class):
     """
     Parameters:
