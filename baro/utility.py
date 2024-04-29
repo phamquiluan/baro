09ee051dccb604748056b45f38c4ef74d9db8d07
Luan Pham <phamquiluan@gmail.com>
2024-04-29 12:18:26 +0000

    Update utils

--- baro/utility.py
+++ baro/utility.py
@@ -135,7 +135,7 @@ def download_data(remote_url=None, local_path=None):
 
     response = requests.get(remote_url, stream=True)
     total_size_in_bytes = int(response.headers.get("content-length", 0))
-    block_size = 1024  # 1 Kibibyte
+    block_size = 1024 # 1 Kibibyte
 
     progress_bar = tqdm(
         desc=f"Downloading {local_path}..",
